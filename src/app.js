3fbdd7801314ef20eda7e244ffa197d6e4912a2a
javier26 <javierne233@gmail.com>
2023-05-10 23:18:44 -0400

    app-libro

--- src/app.js
+++ src/app.js
@@ -9,5 +9,9 @@ const xss= require ("xss-clean")
 const app= express();
 const limit=rateLimit({
   max:10000,
-  windowMs: 60*60*100
+  windowMs: 60*60*1000,
+  message: "too many request fro, thes IP, please try again in one hour!"
 })
+if(process.env.NODE_ENV==="developmen"){
+  app.use(morgan("dev"))
+}
